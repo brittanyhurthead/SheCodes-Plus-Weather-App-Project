517e0813b844eb1600f9b4ab406225fa8e141412
brittanyhurthead <brittanyhurthead@gmail.com>
2023-06-11 11:10:44 -0400

    Updated the Id's
    Updated the Id's and moved the script source to bottom of body

---
+++ app.js
@@ -0,0 +1,19 @@
+const searchForm = document.querySelector("#search-form");
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#search-box");
+  cityElement.innerHTML = cityInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=f8e6a9e3d6fde87cb38868da460b1371&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = temperature + "°F";
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+searchForm.addEventListener("submit", searchCity);
